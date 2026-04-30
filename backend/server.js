0c3a1bf8255df01a83eb7c28ceec9e0836327b42
Avanti Joshi <joshiavanti2004@gmail.com>
2026-04-30 19:53:35 +0530

    Add the new files

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 //app config
 const app = express()
@@ -12,11 +14,11 @@ connectCloudinary()
 
 
 //middlewares
-
 app.use(express.json())
 app.use(cors())
 
 //api endpoints
+app.use('/api/admin',adminRouter)
 
 app.get('/',(req,res) => {
     res.send('API WORKING')
