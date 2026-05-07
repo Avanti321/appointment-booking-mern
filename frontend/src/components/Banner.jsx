642a302cd351be00a268a1c3c227f530442d5480
Avanti Joshi <joshiavanti2004@gmail.com>
2026-05-07 10:55:24 +0530

    Improved UI typography and font styling

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -10,8 +10,8 @@ const Banner = () => {
         {/* -----Left Side------- */}
         <div className='flex-1 py-8 sm:py-10 md:py-16 lg:py-25 lg:pl-5'>
             <div className='text-xl sm:text-2xl md:text-3xl lg:text-5xl font-semibold text-white'>
-                <p>Book Appointment</p>
-                <p className='mt-4'>With 100+ Trusted Doctors</p>
+                <p>Schedule Your Appointment</p>
+                <p className='mt-4'>with 100+ Experienced Doctors</p>
             </div>
             <button onClick={()=>{navigate('/login'); scrollTo(0,0)}} className='bg-white text-sm sm:text-base text-gray-600 px-8 py-3 rounded-full mt-6 hover:scale-105 transition-all'>Create account</button>
         </div>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,47 +2,50 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 const Footer = () => {
-  return (
-    <div className='md:mx-10'>
+    return (
+        <div className='md:mx-10'>
 
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-            {/* --------Left Section------- */}
-            <div>
-                <img className='mb-5 w-40' src={assets.logo} alt=""/>
-                <p className='w-full md:w-2/3 text-gray-600 loading-6'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+                {/* --------Left Section------- */}
+                <div>
+                    <img className='mb-5 w-40' src={assets.logo} alt="" />
+                    <p className='w-full md:w-2/3 text-gray-600 loading-6'>Welcome to HealthQ Dashboard.
+                        Track patient activity, monitor appointments, and analyze data effortlessly.
+                        Designed to simplify workflows and improve productivity.
+                        Your smart healthcare management solution.</p>
 
+                </div>
+
+                {/* --------Center Section------- */}
+                <div>
+                    <p className='text-xl font-medium mb-5'>COMPANY</p>
+                    <ul className='flex flex-col gap-2 text-gray-600'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+
+                </div>
+
+                {/* --------Right Section------- */}
+                <div>
+                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                    <ul>
+                        <li>+91 8796754784</li>
+                        <li>abc@gmail.com</li>
+                    </ul>
+                </div>
             </div>
 
-             {/* --------Center Section------- */}
+            {/* -----Copyrights----- */}
             <div>
-                <p className='text-xl font-medium mb-5'>COMPANY</p>
-                <ul className='flex flex-col gap-2 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy policy</li>
-                </ul>
-                
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2026@ Appointment-Booking-mern All Right Reserved.</p>
             </div>
 
-             {/* --------Right Section------- */}
-            <div>
-                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-                <ul>
-                    <li>+91 8796754784</li>
-                    <li>abc@gmail.com</li>
-                </ul>
-            </div>
         </div>
-
-        {/* -----Copyrights----- */}
-        <div>
-            <hr />
-            <p className='py-5 text-sm text-center'>Copyright 2026@ Appointment-Booking-mern All Right Reserved.</p>
-        </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Footer
